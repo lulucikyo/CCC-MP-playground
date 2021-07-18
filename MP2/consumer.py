a06b42da71abaa5b804a3f289c8eca6c997dd98b
Xi Li <lixi220@outlook.com>
2021-07-18 18:14:47 -0500

    group id

--- MP2/consumer.py
+++ MP2/consumer.py
@@ -5,10 +5,11 @@ from kafka import KafkaAdminClient, KafkaConsumer, KafkaProducer
 from kafka.admin import NewTopic
 
 consumer = KafkaConsumer(bootstrap_servers=['b-1.mp2.uhzy0o.c3.kafka.us-east-1.amazonaws.com:9094','b-2.mp2.uhzy0o.c3.kafka.us-east-1.amazonaws.com:9094','b-3.mp2.uhzy0o.c3.kafka.us-east-1.amazonaws.com:9094'],
-                         auto_offset_reset='latest',
+                         auto_offset_reset='earliest',
                          enable_auto_commit=True,
                          consumer_timeout_ms=10000,
-                         security_protocol="SSL")
+                         security_protocol="SSL",
+                         group_id = "test-group")
 consumer.subscribe(['my-topic'])
 
 for message in consumer:
