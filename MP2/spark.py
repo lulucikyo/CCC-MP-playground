017f66511e35e8e58a6a7e1ebd58d3ce423d1ef2
Xi Li <lixi220@outlook.com>
2021-07-18 22:15:34 -0500

    env

--- MP2/spark.py
+++ MP2/spark.py
@@ -1,11 +1,17 @@
+import findspark
+import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
+
 
 spark = SparkSession \
     .builder \
     .appName("MP2") \
     .getOrCreate()
+
+findspark.init()
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2'
 
 df = spark \
   .readStream \
@@ -13,8 +19,9 @@ df = spark \
   .option("kafka.bootstrap.servers", "b-1.mp2.uhzy0o.c3.kafka.us-east-1.amazonaws.com:9094,b-2.mp2.uhzy0o.c3.kafka.us-east-1.amazonaws.com:9094,b-3.mp2.uhzy0o.c3.kafka.us-east-1.amazonaws.com:9094") \
   .option("subscribe", "my-topic") \
   .option("startingOffsets", "earliest") \
-  .option("kafka.group.id", "str-test") \
   .load()
+
+#.option("kafka.group.id", "str-test") \
 
 df = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
 
