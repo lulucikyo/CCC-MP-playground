670655c15c189e0e0273ac15eee6dbba53db2b09
Xi Li <lixi220@outlook.com>
2021-07-20 00:58:55 -0500

    update

--- MP2/producer.py
+++ MP2/producer.py
@@ -7,7 +7,7 @@ from kafka.admin import NewTopic
 producer = KafkaProducer(bootstrap_servers=['b-1.mp2-2.bd6aae.c3.kafka.us-east-1.amazonaws.com:9092','b-2.mp2-2.bd6aae.c3.kafka.us-east-1.amazonaws.com:9092','b-3.mp2-2.bd6aae.c3.kafka.us-east-1.amazonaws.com:9092'],
                          value_serializer = lambda x: dumps(x).encode('utf-8'))
 
-f = open("final_v2.csv", "r")
+f = open("short.csv", "r")
 f.readline()
 
 count = 0
@@ -26,7 +26,7 @@ for line in f:
             'CRSArrTime':v[9],
             'ArrTime':v[10],
             'ArrDelay':v[11]}
-    producer.send('alldata', value = data)
+    producer.send('test', value = data)
     count += 1
     if count % 10000==0:
         print("Sent {} records".format(count))
