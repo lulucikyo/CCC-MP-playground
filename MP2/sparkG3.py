57e40d872ddff7ae4e94dc350679f17fdde7481a
Xi Li <lixi220@outlook.com>
2021-07-20 11:37:36 -0500

    update

--- MP2/sparkG3.py
+++ MP2/sparkG3.py
@@ -64,100 +64,65 @@ dfnew = dfnew.filter(col("FlightDate").substr(1,4)=="2008")
 dfnew = dfnew.withColumn("ArrDelay", col("ArrDelay").cast("double"))
 dfnew.printSchema()
 
+def foreach_batch_function(dff, batch_id):
+    ### first leg
+    df1 = dff.filter(col("CRSDepTime")<"1200")
+    df1 = df1.select(col("Origin"), col("Dest"), concat(col('UniqueCarrier'),lit(" "),col('FlightNum')).alias("Flight"), 
+                    col("ArrDelay"), 
+                    to_timestamp(concat(col("FlightDate"),col("CRSDepTime")), "yyyy-MM-ddHHmm").alias("CRSDep"), 
+                    to_date(col("FlightDate"), "yyyy-MM-dd").alias("Date"))
+    df1 = df1.alias("l")
+    dfgroupby1 = df1.groupBy("Origin", "Dest", "Date").agg(min("ArrDelay")).alias("ll")
+    cond = [col("l.Origin")==col("ll.Origin"),
+            col("l.Dest")==col("ll.Dest"),
+            col("l.Date")==col("ll.Date"),
+            col("l.ArrDelay")==col("ll.min(ArrDelay)")
+            ]
+    df1 = df1.join(dfgroupby1, cond, "inner")
+    df1.printSchema()
+    df1.show()
 
-df1 = dfnew.filter(col("CRSDepTime")<"1200")
-df1 = df1.select(col("Origin"), col("Dest"), concat(col('UniqueCarrier'),lit(" "),col('FlightNum')).alias("Flight"), 
-                col("ArrDelay"), 
-                to_timestamp(concat(col("FlightDate"),col("CRSDepTime")), "yyyy-MM-ddHHmm").alias("CRSDep"), 
-                to_date(col("FlightDate"), "yyyy-MM-dd").alias("Date"))
-df1 = df1.alias("l")
-dfgroupby1 = df1.groupBy("Origin", "Dest", "Date").agg(min("ArrDelay")).alias("ll")
-cond = [col("l.Origin")==col("ll.Origin"),
-        col("l.Dest")==col("ll.Dest"),
-        col("l.Date")==col("ll.Date"),
-        col("l.ArrDelay")==col("ll.min(ArrDelay)")
-        ]
-df1 = df1.join(dfgroupby1, cond, "inner")
-df1.printSchema()
+    #second leg
+    df2 = dff.filter(col("CRSDepTime")>"1200")
+    df2 = df2.select(col("Origin"), col("Dest"), concat(col('UniqueCarrier'),lit(" "),col('FlightNum')).alias("Flight"), 
+                    col("ArrDelay"), 
+                    to_timestamp(concat(col("FlightDate"),col("CRSDepTime")), "yyyy-MM-ddHHmm").alias("CRSDep"), 
+                    to_date(col("FlightDate"), "yyyy-MM-dd").alias("Date"))
+    df2 = df2.alias("r")
+    dfgroupby2 = df2.groupBy("Origin", "Dest", "Date").agg(min("ArrDelay")).alias("rr")
+    cond = [col("r.Origin")==col("rr.Origin"),
+            col("r.Dest")==col("rr.Dest"),
+            col("r.Date")==col("rr.Date"),
+            col("r.ArrDelay")==col("rr.min(ArrDelay)")
+            ]
+    df2 = df2.join(dfgroupby2, cond, "inner")
+    df2.printSchema()
+    df2.show()
 
-"""df2 = dfnew.filter(col("CRSDepTime")>"1200")
-df2 = df2.select(col("Origin"), col("Dest"), concat(col('UniqueCarrier'),lit(" "),col('FlightNum')).alias("Flight"), 
-                col("ArrDelay"), 
-                to_timestamp(concat(col("FlightDate"),col("CRSDepTime")), "yyyy-MM-ddHHmm").alias("CRSDep"), 
-                to_date(col("FlightDate"), "yyyy-MM-dd").alias("Date"))
-df2 = df2.alias("r")
-dfgroupby2 = df2.groupBy("Origin", "Dest", "Date").agg(min("ArrDelay")).alias("rr")
-cond = [col("r.Origin")==col("rr.Origin"),
-        col("r.Dest")==col("rr.Dest"),
-        col("r.Date")==col("rr.Date"),
-        col("r.ArrDelay")==col("rr.min(ArrDelay)")
-        ]
-df2 = df2.join(dfgroupby2, cond, "inner")
-df2.printSchema()"""
+    # final join
+    cond = [col("l.Dest")==col("r.Origin"), datediff(col("r.CRSDep"), col("l.CRSDep"))==2]
+    dfjoin = df1.join(df2, cond, "inner")
 
-query6_1 = (
-    df1.writeStream \
-    .outputMode("append").option("truncate", "true") \
-    .format("console") \
-    .start()
-)
+    df3 = dfjoin.select("l.Origin", "l.Dest","l.Flight", \
+                        date_format(col("l.CRSDep"),"HH:mm dd/MM/yyyy").alias("l.CRSDep"), \
+                        "l.ArrDelay", "r.Origin", "r.Dest","r.Flight", \
+                        date_format(col("r.CRSDep"),"HH:mm dd/MM/yyyy").alias("r.CRSDep"), \
+                        "r.ArrDelay", \
+                        (col("l.ArrDelay")+col("r.ArrDelay")).alias("TotDelay")) 
 
-"""query6_2 = (
-    df2.writeStream \
-    .outputMode("append").option("truncate", "true") \
-    .format("console") \
-    .start()
-)
-"""
-stop_stream_query(query6_1, 5)
-"""stop_stream_query(query6_2, 5)"""
+    df3.show(10)
+    # df3_1 = df3.where("l.Origin=='BOS' and l.Dest=='ATL' and r.Dest=='LAX' and l.CRSDep LIKE '%03/04/2008'")
+    #df3_2 = df3.where("l.Origin=='PHX' and l.Dest=='JFK' and r.Dest=='MSP' and l.CRSDep LIKE '%07/09/2008'")
+    #df3_3 = df3.where("l.Origin=='DFW' and l.Dest=='STL' and r.Dest=='ORD' and l.CRSDep LIKE '%24/01/2008'")
+    #df3_4 = df3.where("l.Origin=='LAX' and l.Dest=='MIA' and r.Dest=='LAX' and l.CRSDep LIKE '%16/05/2008'")
 
-"""cond = [col("l.Dest")==col("r.Origin"), datediff(col("r.CRSDep"), col("l.CRSDep"))==2]
-dfjoin = df1.join(df2, cond, "inner")
 
-df3 = dfjoin.select("l.Origin", "l.Dest","l.Flight", \
-                    date_format(col("l.CRSDep"),"HH:mm dd/MM/yyyy").alias("l.CRSDep"), \
-                    "l.ArrDelay", "r.Origin", "r.Dest","r.Flight", \
-                    date_format(col("r.CRSDep"),"HH:mm dd/MM/yyyy").alias("r.CRSDep"), \
-                    "r.ArrDelay", \
-                    (col("l.ArrDelay")+col("r.ArrDelay")).alias("TotDelay")) """
-
-"""
-df3_1 = df3.where("l.Origin=='BOS' and l.Dest=='ATL' and r.Dest=='LAX' and l.CRSDep LIKE '%03/04/2008'")
-df3_2 = df3.where("l.Origin=='PHX' and l.Dest=='JFK' and r.Dest=='MSP' and l.CRSDep LIKE '%07/09/2008'")
-df3_3 = df3.where("l.Origin=='DFW' and l.Dest=='STL' and r.Dest=='ORD' and l.CRSDep LIKE '%24/01/2008'")
-df3_4 = df3.where("l.Origin=='LAX' and l.Dest=='MIA' and r.Dest=='LAX' and l.CRSDep LIKE '%16/05/2008'")
-
-query6_1 = (
-    df3_1.writeStream \
-    .outputMode("complete").option("truncate", "false") \
-    .format("console") \
-    .start()
-)
-
-query6_2 = (
-    df3_2.writeStream \
-    .outputMode("complete").option("truncate", "false") \
-    .format("console") \
-    .start()
-)
-
-query6_3 = (
-    df3_4.writeStream \
-    .outputMode("complete").option("truncate", "false") \
-    .format("console") \
-    .start()
-)
-
-query6_4 = (
-    df3_4.writeStream \
-    .outputMode("complete").option("truncate", "false") \
-    .format("console") \
-    .start()
+query = (
+    dfnew.writeStream \
+        .foreachBatch(foreach_batch_function)
+        .outputMode("append") \
+        .start()
 )
 
 
-stop_stream_query(query6_1, 5)
-stop_stream_query(query6_2, 5)
-stop_stream_query(query6_3, 5)
-stop_stream_query(query6_4, 5)"""
+stop_stream_query(query, 5)
